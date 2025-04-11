89f8ff8cb3dba3da67465f4e3316b24ae9752557
Anthony <tsims49@gmail.com>
2025-04-11 12:07:47 -0500

    Add Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
