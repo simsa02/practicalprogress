05164776f4917a0709ae7799ac78db2af0a96d1c
Anthony <tsims49@gmail.com>
2025-04-24 10:00:53 -0500

    Share Card 12

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,28 @@
 // pages/_document.js
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {/* Facebook App ID for Open Graph */}
+          <meta
+            property="fb:app_id"
+            content={process.env.NEXT_PUBLIC_FB_APP_ID}
+          />
+          {/* (Optional) Default OG tags you want on every page */
+          /*  You can also pull in your SITE_URL env var here if needed */} 
+          <meta
+            property="og:site_name"
+            content="Practical Progress"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
